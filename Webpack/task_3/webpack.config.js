204c2c6fb9dd71a4d3bfc79c6f222b14b467e9b6
S1even <Stevendevin95520@hotmail.com>
2026-07-27 10:35:31 +0200

    Dev servers, modules, and tree shaking

---
+++ Webpack/task_3/webpack.config.js
@@ -0,0 +1,51 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  entry: {
+    header: './modules/header/header.js',
+    body: './modules/body/body.js',
+    footer: './modules/footer/footer.js',
+  },
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'public'),
+  },
+  devServer: {
+    static: path.resolve(__dirname, 'public'),
+    port: 8564,
+    open: true,
+  },
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Holberton Dashboard',
+    }),
+  ],
+  performance: {
+    maxAssetSize: 1000000,
+    maxEntrypointSize: 1000000,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        // webpack 5 native asset modules: no file-loader / image-webpack-loader,
+        // so no native binaries to download at install time (reliable on CI).
+        test: /\.(gif|png|jpe?g|svg)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+};
