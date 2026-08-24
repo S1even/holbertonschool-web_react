8cccf498420941d0f9a0e9217815140ec5843146
S1even <Stevendevin95520@hotmail.com>
2026-08-24 11:35:57 +0200

    Basic components

---
+++ react_props/task_0/dashboard/src/Notifications/Notifications.jsx
@@ -0,0 +1,34 @@
+import './Notifications.css'
+import closeButton from '../assets/close-button.png'
+import { getLatestNotification } from '../utils/utils'
+
+function Notifications() {
+  return (
+    <div className="notification-items">
+      <button
+        aria-label="Close"
+        onClick={() => console.log('Close button has been clicked')}
+        style={{
+          float: 'right',
+          background: 'transparent',
+          border: 'none',
+          cursor: 'pointer',
+          padding: 0,
+        }}
+      >
+        <img src={closeButton} alt="close" width="12" height="12" />
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <li data-priority="default">New course available</li>
+        <li data-priority="urgent">New resume available</li>
+        <li
+          data-priority="urgent"
+          dangerouslySetInnerHTML={{ __html: getLatestNotification() }}
+        />
+      </ul>
+    </div>
+  )
+}
+
+export default Notifications
